10439c2906fce3b712a73354273b6209d6e6002c
A.S. Aziz <asazizmn@gmail.com>
2021-03-15 16:19:39 +0800

    remove unnecessary title display and format code

--- src/ChatWindow.js
+++ src/ChatWindow.js
@@ -15,23 +15,23 @@ class ChatWindow extends Component {
 
     render() {
         return (
-
             <div className="chat-window">
 
-                <h2>Super Awesome Chat</h2>
                 <div className="name sender">{this.props.users[this.props.idx].username}</div>
 
                 <ul className="message-list">
-                    {this.props.messages.map((message, index) => (
-                        <li
-                            key={index}
-                            className={
-                                message.username === this.props.users[this.props.idx].username ? 'message sender' : 'message recipient'
-                            }
-                        >
-                            <p>{`${message.username}: ${message.text}`}</p>
-                        </li>
-                    ))}
+                    {
+                        this.props.messages.map((message, index) => (
+                            <li
+                                key={index}
+                                className={
+                                    message.username === this.props.users[this.props.idx].username ? 'message sender' : 'message recipient'
+                                }
+                            >
+                                <p>{`${message.username}: ${message.text}`}</p>
+                            </li>
+                        ))
+                    }
                 </ul>
 
                 <div>
